3b5c7c34fd205d44a29f73d0909bfcb93c3355ec
mott <mott@student.42heilbronn.de>
2025-03-29 20:39:13 +0100

    setup my first fastify server

--- code/html/src/server.ts
+++ code/html/src/server.ts
@@ -1,20 +1,85 @@
-// Import the framework and instantiate it
+// import Fastify, {FastifyInstance, FastifyReply, FastifyRequest, RouteShorthandOptions} from 'fastify';
 
+// const server: FastifyInstance = Fastify({ logger: true });
+
+// const opts: RouteShorthandOptions = {
+//     schema: {
+//       response: {
+//         200: {
+//           type: 'object',
+//           properties: {
+//             pong: {
+//               type: 'string'
+//             }
+//           }
+//         }
+//       }
+//     }
+//   };
+
+// server.get('/ping', opts, async (request: FastifyRequest, reply: FastifyReply) => {
+//     return { pong: "Hello World!" };
+// });
+
+// const start = async (): Promise<void> => {
+//     try {
+//         await server.listen({ port: 3000, host: '127.0.0.1' });
+//     }
+//     catch (err) {
+//         server.log.error(err);
+//         process.exit(1);
+//     }
+// };
+
+// start();
+
+
+
+// // Require the framework and instantiate it
+
+// // ESM
+// import Fastify from 'fastify'
+
+// const fastify = Fastify({
+//   logger: true
+// })
+
+// // Declare a route
+// fastify.get('/', function (request, reply) {
+//   reply.send({ hello: 'world' })
+// })
+
+// // Run the server!
+// fastify.listen({ port: 3000 }, function (err, address) {
+//   if (err) {
+//     fastify.log.error(err)
+//     process.exit(1)
+//   }
+//   // Server is now listening on ${address}
+// })
+
+
+
+// ESM
 import Fastify from 'fastify'
 
 const fastify = Fastify({
   logger: true
 })
 
-// Declare a route
-fastify.get('/', async function handler (request, reply) {
+fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
 })
 
-// Run the server!
-try {
-  await fastify.listen({ port: 3000 })
-} catch (err) {
-  fastify.log.error(err)
-  process.exit(1)
+/**
+ * Run the server!
+ */
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000, host: '0.0.0.0' })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
 }
+start()
