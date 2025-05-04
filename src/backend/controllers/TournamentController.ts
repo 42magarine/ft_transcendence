54c281f19ff7953f8aa3f34f8d27138793f88ed4
mleibeng <mleibeng@student.42heilbronn.de>
2025-05-04 05:50:02 +0200

    noch bisschen angefangen mit rumgespielen und so

--- src/backend/controllers/TournamentController.ts
+++ src/backend/controllers/TournamentController.ts
@@ -1,17 +1,24 @@
-import { Player } from "../gamelogic/Player.js";
+import { Player } from "../gamelogic/components/Player.js";
+import { TournamentLobby } from "../lobbies/TournamentLobby.js";
+import { GameService } from "../services/GameService.js";
 import { MessageHandlers } from "../services/MessageHandlers.js";
+import { TournamentService } from "../services/TournamentService.js";
+import { UserService } from "../services/UserService.js";
+import { MatchController } from "./MatchController.js";
 
-export class TournamentController {
+export class TournamentController extends MatchController{
     private _lobbies: Map<string, TournamentLobby>;
     private _clients: Map<WebSocket, Player | null>;
-    private _handlers: TournamentMessageHandlers;
+    private _handlers: MessageHandlers;
     private _tournamentService: TournamentService;
 
     constructor() {
-        this._tournamentService = new TournamentService(new TournamentRepository(), new UserService());
+        const coolUserService = new UserService()
+        super(coolUserService, )
+        this._tournamentService = new TournamentService(coolUserService, new GameService(coolUserService));
         this._lobbies = new Map<string, TournamentLobby>();
         this._clients = new Map<WebSocket, Player | null>;
-        this._handlers = new TournamentMessageHandlers(this.tournamentBroadcast.bind(this));
+        this._handlers = new MessageHandlers(this._broadcast.bind(this));
     }
 
     public handleConnection = (connection: WebSocket, userId?:number): void => {
