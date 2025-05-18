446e1566fbf19160d9160f19e9c9a24222c34238
mott <mott@student.42heilbronn.de>
2025-05-18 12:42:32 +0200

    update module list

--- src/tailwind.config.cjs
+++ src/tailwind.config.cjs
@@ -1,12 +1,12 @@
 module.exports = {
-	content: [
-		"./frontend/**/*.{html,js,ts,jsx,tsx}",
-		"./utils/**/*.{js,ts}",
-		"./*.{html,js,ts}",
-		"./index.html"
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: [
+        "./frontend/**/*.{html,js,ts,jsx,tsx}",
+        "./utils/**/*.{js,ts}",
+        "./*.{html,js,ts}",
+        "./index.html"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
