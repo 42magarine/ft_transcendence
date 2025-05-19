4c5463f90f4747e44757fc99991a5df15c991519
diggens42 <frederic.marcel.wahl@gmail.com>
2025-05-19 16:04:05 +0200

    ws wrapper etc

--- src/frontend/services/LobbyService.ts
+++ src/frontend/services/LobbyService.ts
@@ -4,195 +4,6 @@ import Router from '../../utils/Router.js';
 
 class LobbyService {
 
-// 	private socket: any;
-// 	private userService: UserService;
-// 	private currentUser: any = null;
-// 	private lobbyData: LobbyInfo[] = [];
-// 	private onLobbyListUpdated: (() => void)[] = [];
-
-// 	constructor() {
-// 		this.userService = new UserService();
-// 	}
-
-// 	public initSocket() {
-// 		this.socket = new WebSocket('wss://localhost:3000/game/wss');
-// 		// this.socket = new WebSocket("wss://10.12.6.7:3000/game/wss");
-// 		this.fuckYouWebsocket(this.socket).then(() => {
-// 			console.log('Connected to WebSocket server');
-// 		});
-
-// 		this.socket.addEventListener('message', (event: MessageEvent<string>) => {
-// 			const data: ServerMessage = JSON.parse(event.data);
-
-// 			if (data.type === 'lobbyCreated') {
-// 				const lobbyId = data.lobbyId;
-// 				if (lobbyId) {
-// 					Router.redirect(`/lobby/${lobbyId}`);
-// 				}
-// 			}
-
-
-// 			// if (data.type === 'lobbyList') {
-// 			// 	this.lobbyData = data.lobbies!;
-// 			// 	this.onLobbyListUpdated.forEach(cb => cb());
-// 			// }
-
-// 			// // if (data.type === 'assignPlayer') {
-// 			// // }
-
-// 			// // if (data.type === 'lobbyInfo') {
-// 			// // }
-
-// 			// if (data.type === 'playerJoined') {
-// 			// 	const lobbyId = data.lobbyId;
-// 			// 	if (window.location.pathname === `/lobby/${lobbyId}`) {
-// 			// 		const liveUpdateEvent = new CustomEvent('LobbyPlayerJoined', {
-// 			// 			bubbles: true,
-// 			// 			detail: { lobbyId }
-// 			// 		});
-// 			// 		document.dispatchEvent(liveUpdateEvent);
-// 			// 	}
-// 			// }
-
-// 			// // if (data.type === 'playerDisconnected') {
-// 			// // }
-
-// 			// if (data.type === 'gameStarted') {
-// 			// 	const lobbyId = data.lobbyId;
-// 			// 	if (lobbyId) {
-// 			// 		window.history.pushState({}, '', `/pong/${lobbyId}`);
-// 			// 		document.dispatchEvent(new Event('popstate'));
-// 			// 	}
-// 			// }
-
-// 			// if (data.type === 'inviteReceived') {
-// 			// 	const { lobbyId, userId } = data;
-// 			// 	document.dispatchEvent(new CustomEvent('LobbyInviteReceived', {
-// 			// 		bubbles: true,
-// 			// 		detail: { lobbyId, userId }
-// 			// 	}));
-// 			// }
-// 		});
-// 	}
-
-// 	public registerLobbyListListener(callback: () => void) {
-// 		this.onLobbyListUpdated.push(callback);
-// 	}
-
-// 	private fuckYouWebsocket(socket: WebSocket): Promise<void> {
-// 		return new Promise((resolve, reject) => {
-// 			if (socket.readyState === WebSocket.OPEN) {
-// 				resolve();
-// 			} else {
-// 				socket.addEventListener('open', () => resolve(), { once: true });
-// 				socket.addEventListener('error', () =>
-// 					reject(new Error('Websocket is shit fuck you websocket')), { once: true });
-// 			}
-// 		});
-// 	}
-
-// 	public safeSend(msg: ClientMessage) {
-// 		if (this.socket.readyState === WebSocket.OPEN) {
-// 			this.socket.send(JSON.stringify(msg));
-// 		} else {
-// 			console.warn('Tried to send message but socket not open:', msg);
-// 		}
-// 	}
-
-// 	public setupEventListeners() {
-// 		const createBtn = document.getElementById('createLobbyBtn') as HTMLElement | null;
-// 		if (createBtn) {
-// 			createBtn.addEventListener('click', async (e) => {
-// 				console.log("createBtn clicked")
-// 				e.preventDefault();
-
-// 				// Call the service method to create a lobby (uses existing WebSocket logic)
-// 				this.createLobby();
-// 			});
-// 		}
-// 	}
-
-// 	public async createLobby() {
-// 		this.currentUser = await this.userService.getCurrentUser();
-// 		if (!this.currentUser) return;
-
-// 		const msg: ClientMessage = {
-// 			type: 'createLobby',
-// 			userId: this.currentUser.id,
-// 		};
-// 		this.safeSend(msg);
-// 	}
-
-// 	public async joinGame(lobbyId: string) {
-// 		// Logic to join a game
-// 		const msg = {
-// 			type: 'joinLobby',
-// 			lobbyId,
-// 		};
-// 		this.safeSend(msg);
-// 	}
-
-// 	public getLobbyList(): LobbyInfo[] {
-// 		const msg: ClientMessage = {
-// 			type: 'getLobbyList',
-// 			lobbies: this.currentUser.id,
-// 		};
-// 		this.safeSend(msg);
-// 		return this.lobbyData || [];
-// 	}
-
-// 	public initialize(): void {
-// 		document.addEventListener('RouterContentLoaded', () => {
-// 			this.initSocket();
-// 			this.setupEventListeners();
-// 		});
-// 	}
-
-// 	public startGame(lobbyId: string) {
-// 		const msg: ClientMessage = {
-// 			type: 'startGame',
-// 			lobbyId,
-// 		};
-// 		this.safeSend(msg);
-// 	}
-
-// 	public markReady(userID: string, lobbyId: string) {
-// 		const msg: ClientMessage = {
-// 			type: 'playerReady',
-// 			userID,
-// 			lobbyId,
-// 		};
-// 		this.safeSend(msg);
-// 	}
-
-// 	public leaveLobby(lobbyId: string) {
-// 		const msg: ClientMessage = {
-// 			type: 'leaveLobby',
-// 			lobbyId,
-// 		};
-// 		this.safeSend(msg);
-// 	}
-
-// 	public sendInvite(userID: string, lobbyId: string) {
-// 		const msg: ClientMessage = {
-// 			type: 'sendInvite',
-// 			userID,
-// 			lobbyId,
-// 		};
-// 		this.safeSend(msg);
-// 	}
-
-// 	public acceptInvite(userID: string, lobbyId: string) {
-// 		const msg: ClientMessage = {
-// 			type: 'acceptInvite',
-// 			userID,
-// 			lobbyId,
-// 		};
-// 		this.safeSend(msg);
-// 	}
-  
-  
-
     private socket: any;
     private userService: UserService;
     private currentUser: any = null;
@@ -204,9 +15,10 @@ class LobbyService {
     }
 
     public initSocket() {
-        this.socket = new WebSocket(`const host = window.location.host; // IP Adresse im 42 Netzwerk
-`);
-        this.fuckYouWebsocket(this.socket).then(() => {
+//         this.socket = new WebSocket(`const host = window.location.host; // IP Adresse im 42 Netzwerk
+// `);
+        this.socket = new WebSocket("wss://localhost:3000/game/wss")
+        this.webSocketWrapper(this.socket).then(() => {
             console.log('Connected to WebSocket server');
         });
 
@@ -216,21 +28,17 @@ class LobbyService {
             if (data.type === 'lobbyCreated') {
                 const lobbyId = data.lobbyId;
                 if (lobbyId) {
+                    this.requestLobbyList();
                     Router.redirect(`/lobby/${lobbyId}`);
                 }
             }
 
-
             if (data.type === 'lobbyList') {
                 this.lobbyData = data.lobbies!;
+                console.log(this.lobbyData)
                 this.onLobbyListUpdated.forEach(cb => cb());
             }
 
-            // if (data.type === 'assignPlayer') {
-            // }
-
-            // if (data.type === 'lobbyInfo') {
-            // }
 
             if (data.type === 'playerJoined') {
                 const lobbyId = data.lobbyId;
@@ -243,8 +51,6 @@ class LobbyService {
                 }
             }
 
-            // if (data.type === 'playerDisconnected') {
-            // }
 
             if (data.type === 'gameStarted') {
                 const lobbyId = data.lobbyId;
@@ -262,6 +68,15 @@ class LobbyService {
                     detail: { lobbyId, userId }
                 }));
             }
+
+            // if (data.type === 'assignPlayer') {
+            // }
+
+            // if (data.type === 'lobbyInfo') {
+            // }
+
+            // if (data.type === 'playerDisconnected') {
+            // }
         });
     }
 
@@ -269,7 +84,7 @@ class LobbyService {
         this.onLobbyListUpdated.push(callback);
     }
 
-    private fuckYouWebsocket(socket: WebSocket): Promise<void> {
+    private webSocketWrapper(socket: WebSocket): Promise<void> {
         return new Promise((resolve, reject) => {
             if (socket.readyState === WebSocket.OPEN) {
                 resolve();
@@ -374,6 +189,14 @@ class LobbyService {
         };
         this.safeSend(msg);
     }
+
+    public requestLobbyList(): void {
+        const msg: ClientMessage = {
+            type: 'getLobbyList',
+        };
+        this.safeSend(msg);
+    }
+
 }
 
 const lobbyService = new LobbyService();
